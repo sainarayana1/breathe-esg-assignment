48eea8411e486b0dc2197b195edf305148042c73
sainarayana1 <mamidipakalakshmisainarayana@gmail.com>
2026-05-28 14:13:16 +0530

    Update companies_home response to include status

--- companies/urls.py
+++ companies/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.http import JsonResponse
 
+
 def companies_home(request):
     return JsonResponse({
+        "status": "success",
         "message": "Companies API Working"
     })
+
 
 urlpatterns = [
     path('', companies_home),
