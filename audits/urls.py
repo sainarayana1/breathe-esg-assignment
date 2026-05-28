29c8f2fb0c5da3585e4b37be79ea59b588c61dc8
sainarayana1 <mamidipakalakshmisainarayana@gmail.com>
2026-05-28 14:13:48 +0530

    Update audits_home response to include status

--- audits/urls.py
+++ audits/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.http import JsonResponse
 
+
 def audits_home(request):
     return JsonResponse({
+        "status": "success",
         "message": "Audits API Working"
     })
+
 
 urlpatterns = [
     path('', audits_home),
