1e70adc39da69254f7aae9c415583db0af787f94
sainarayana1 <mamidipakalakshmisainarayana@gmail.com>
2026-05-28 14:14:32 +0530

    Update ingestion_home response to include status

--- ingestion/urls.py
+++ ingestion/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.http import JsonResponse
 
+
 def ingestion_home(request):
     return JsonResponse({
-        "message": "Ingestion API Working"
+        "status": "success",
+        "message": "Data Ingestion API Working"
     })
+
 
 urlpatterns = [
     path('', ingestion_home),
